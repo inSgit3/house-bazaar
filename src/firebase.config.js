7ce30d0cc7a94675e5d66ca8302782bb0dc001a4
aburashidakbarov <abdurshd@icloud.com>
2023-05-05 18:42:54 +0900

    created starting files

---
+++ src/firebase.config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD95TQVq-F1pOYL9Zd-cuMWc7AteLwVOP8",
+  authDomain: "housebazaar-59524.firebaseapp.com",
+  projectId: "housebazaar-59524",
+  storageBucket: "housebazaar-59524.appspot.com",
+  messagingSenderId: "698484476853",
+  appId: "1:698484476853:web:66492e4a1e484dcf6367fc",
+  measurementId: "G-7PMTMQ8ZXX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore();
---
+++ src/pages/Signin.jsx
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+
+
+const Signin = () => {
+  return (
+    <div>
+      <h1>Sign in</h1>
+    </div>
+  )
+}
+
+export default Signin
